51f7d72d2caa5dea478b8d4bd324858c0be33630
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2025-12-30 13:58:01 +0530

    feat: Implement Redis caching for user data retrieval and add necessary Redis configuration and dependencies.

---
+++ socialhub-backend/src/config/redis.ts
@@ -0,0 +1,14 @@
+import Redis from "ioredis";
+
+export const redis = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+redis.on("connect", () => {
+  console.log("Redis Connected 🚀");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis Error", err);
+});
--- socialhub-backend/src/controllers/user.controller.ts
+++ socialhub-backend/src/controllers/user.controller.ts
@@ -1,12 +1,33 @@
 import { Response } from "express";
 import User from "../models/User";
+import { redis } from "../config/redis";
 import { AuthRequest } from "../middlewares/auth.middleware";
+import { ApiError } from "../utils/ApiError";
 
 export const getMe = async (req: AuthRequest, res: Response) => {
+   const cacheKey = `user:${req.userId}`;
+
+   // 🔥 1. Check cache
+  const cachedUser = await redis.get(cacheKey);
+  if (cachedUser) {
+    return res.json({
+      success: true,
+      user: JSON.parse(cachedUser),
+      source: "cache"
+    });
+  }
+
+  // 🔥 2. DB call
   const user = await User.findById(req.userId).select("-password");
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+ // 🔥 3. Save to cache (TTL = 60 sec)
+  await redis.setex(cacheKey, 60, JSON.stringify(user));
 
   res.json({
     success: true,
     user,
+    source: "db"
   });
 };
